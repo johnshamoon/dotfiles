b763fe1588ad60b8c99d32d7b81060765b3c2b1f
John Shamoon <johnshamoon96@gmail.com>
2019-01-27 14:25:59 -0500

    Fix Vundle Setup and Add Auto PluginInstall

--- extract.sh
+++ extract.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+# copy all of the files to the home directory
 cp -r .vim ~/
 cp .vimrc ~/
 cp .bashrc ~/
 cp .bash_aliases ~/
 cp .tmux.conf ~/
+
+# Download Vundle
+# TODO: Add submodule instead instead of doing it here.
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Open vim, run :PluginInstall, then quit
+vim +PluginInstall +qall

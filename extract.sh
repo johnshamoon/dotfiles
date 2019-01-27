a1f1aea98fd3bf4d00a815f7d3fb8f19ef92f325
John Shamoon <johnshamoon96@gmail.com>
2019-01-27 14:30:31 -0500

    Automate PluginInstall

--- extract.sh
+++ extract.sh
@@ -4,3 +4,10 @@ cp .vimrc ~/
 cp .bashrc ~/
 cp .bash_aliases ~/
 cp .tmux.conf ~/
+
+# Make sure that we got the Vundle submodule
+git submodule update --recursive
+git pull --recurse-submodules
+
+# Install vim plugins
+vim +PluginInstall +qall

dfee10f2d40ebfb3579589a6df1e68c62f0ed357
John Shamoon <johnshamoon96@gmail.com>
2026-04-20 16:31:28 -0700

    replace noice with tiny-cmdline

---
+++ nvim/lua/plugins/tiny-cmdline.lua
@@ -0,0 +1,9 @@
+require("vim._core.ui2").enable({})
+
+return {
+  "rachartier/tiny-cmdline.nvim",
+  opts = {
+    -- '/' and '?' are disabled by default. Recenter them.
+    native_types = {},
+  },
+}

e57d5d0330c624ab49e5319b1824e91b0347bb5a
John Shamoon <johnshamoon@google.com>
2026-04-21 09:47:59 -0700

    update treesitter branch to main

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,23 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     build = ":TSUpdate",
     config = function()
       local configs = require("nvim-treesitter.config")
       configs.setup({
-        ensure_installed = { "fidl", "gn", "json5", "lua", "c", "cpp", "rust", "vim", "java", "python" },
+        ensure_installed = {
+          "fidl",
+          "gn",
+          "json5",
+          "lua",
+          "c",
+          "cpp",
+          "rust",
+          "vim",
+          "java",
+          "python",
+        },
         highlight = {
           enable = true,
           disable = { "cpp", "rust" },
